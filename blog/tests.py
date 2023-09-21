cd24ff78891ff868c8a5a64f53d80c63f5c0de3a
ZeusOnline1998 <zeusonline1998@gmail.com>
2023-09-21 15:28:47 +0530

    Updated Test

--- blog/tests.py
+++ blog/tests.py
@@ -5,8 +5,14 @@ from .models import Post, Comment, User
 class BlogPost(TestCase):
 
     def setUp(self):
+        user = User(email='kilocycl3@gmail')
+        user.set_password('Hello123@')
+        user.save()
 
-        Post.objects.create(title='My Title', content='Description', author=1)
+        user = User.objects.get(id=1)
+        Post.objects.create(title='My Title', content='Description', author=user)
 
     def test_this(self):
-        self.assertEqual(1, 1)
+        p = Post.objects.get(title='My Title')
+        self.assertEqual(p.content, 'Description')
+
