c1642860a909ab64f2d94309ce46c086cf1a92db
ZeusOnline1998 <zeusonline1998@gmail.com>
2023-09-21 15:21:19 +0530

    Added dummy test for github action

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Post, Comment, User
 
 # Create your tests here.
+class BlogPost(TestCase):
+
+    def setUp(self):
+
+        Post.objects.create(title='My Title', content='Description', author=1)
+
+    def test_this(self):
+        self.assertEqual(1, 1)
